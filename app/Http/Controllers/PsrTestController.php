0ef9d7e88dd3745baf04484900b0e9cfbefd0aaf
Gayan Sanjeewa <iamgayan@gmail.com>
2017-08-23 12:42:03 +0530

    Unformat controller

---
+++ app/Http/Controllers/PsrTestController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class PsrTestController extends Controller {
+    public function index() {
+        $condition = true;
+        if ($condition)
+        {
+            // some code here
+        }
+    }
+}
